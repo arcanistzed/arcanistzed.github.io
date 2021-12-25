8cda590d1984d5b1d67837fa9eda27dfacc7d18a
arcanist <arcanistzed@gmail.com>
2021-12-25 17:54:07 -0500

    fix: add square & lower blur
    Signed-off-by: arcanist <arcanistzed@gmail.com>

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 const colors = ["Transparent", "Navy", "DarkBlue", "Blue", "Teal", "DarkCyan", "MidnightBlue", "RoyalBlue", "DarkSlateBlue", "Indigo", "CornflowerBlue"];
-document.querySelector("#gradient").style.backgroundImage = `linear-gradient(0deg, ${colors[Math.round(Math.random() * colors.length)]}, ${colors[Math.round(Math.random() * colors.length)]})`
+document.querySelector("#gradient").style.backgroundImage = `linear-gradient(0deg, ${colors[Math.round(Math.random() * colors.length)]}, ${colors[Math.round(Math.random() * colors.length)]})`;
+document.querySelector("#square").style.backgroundColor = colors[Math.round(Math.random() * colors.length)];
