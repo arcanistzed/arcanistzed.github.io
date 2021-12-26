6630d7da1f42e780fbc4f3e73e95e64f7ae11897
arcanist <arcanistzed@gmail.com>
2021-12-25 19:19:21 -0500

    feat: footer and more dynamic color generation
    Signed-off-by: arcanist <arcanistzed@gmail.com>

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-const colors = ["Transparent", "Navy", "DarkBlue", "Blue", "Teal", "DarkCyan", "MidnightBlue", "RoyalBlue", "DarkSlateBlue", "Indigo", "CornflowerBlue"];
-document.querySelector("#gradient").style.backgroundImage = `linear-gradient(0deg, ${colors[Math.round(Math.random() * colors.length)]}, ${colors[Math.round(Math.random() * colors.length)]})`;
-document.querySelector("#square").style.backgroundColor = colors[Math.round(Math.random() * colors.length)];
+const colors = [];
+
+const one = generateHue(Math.floor(Math.random() * 360));
+const two = generateHue(one);
+const three = generateHue(one, two);
+
+colors.push(one, two, three);
+
+document.querySelector("#gradient").style.backgroundImage = `linear-gradient(
+                hsl(${colors[0]} 100% 10%),
+                hsl(${colors[1]} 100% 10%)
+                )`;
+document.querySelector("#square").style.backgroundColor = `hsl(${colors[2]} 100% 10%)`;
+
+function allAllowedHues(bases) {
+    return Array.from(new Array(360)) // Hue is in degrees
+        .map((_, i) => i + 1) // Insert numbers into the Array from 1 to 300
+        .filter(h => // Filter the Array
+            Math.abs(bases - h) > 15 // Must be sufficiently different than the base
+            && Math.abs(bases - h) <= 100 // Must not be too different
+            && (h < 50 || h > 150) // Must not be green
+        );
+};
+
+function generateHue(...bases) {
+    const validHues = allAllowedHues(bases);
+    const index = Math.floor(Math.random() * validHues.length);
+    return allAllowedHues(bases)[index];
+};
