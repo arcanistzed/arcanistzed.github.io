194523de1c3b7ea364d03d2cd24f614576ee3152
arcanist <arcanistzed@gmail.com>
2021-12-25 21:23:43 -0500

    feat: reorg, jsdoc, & variables
    Signed-off-by: arcanist <arcanistzed@gmail.com>

--- script.js
+++ script.js
@@ -1,29 +1,54 @@
-const colors = [];
+/**
+ * Generate random colors
+ * @returns {number[]} Random colors
+ */
+function generateColors() {
+    const one = generateHue(Math.floor(Math.random() * 360));
+    const two = generateHue(one);
+    const three = generateHue(one, two);
+    return [one, two, three];
+};
 
-const one = generateHue(Math.floor(Math.random() * 360));
-const two = generateHue(one);
-const three = generateHue(one, two);
+/**
+ * Apply the given colors to the page
+ * @param {number[]} colors - Colors to apply
+ */
+function applyColors(colors) {
+    const rootStyle = document.documentElement.style;
+    rootStyle.setProperty("--gradient-color", `
+        linear-gradient(
+            hsl(${colors[0]} 100% 10%),
+            hsl(${colors[1]} 100% 10%)
+        )`);
+    rootStyle.setProperty("--square-color", `hsl(${colors[2]} 100% 10%)`);
+    rootStyle.setProperty("--alt-text-color", `hsl(${colors[2]} 100% 90%)`);
+};
 
-colors.push(one, two, three);
-
-document.querySelector("#gradient").style.backgroundImage = `linear-gradient(
-                hsl(${colors[0]} 100% 10%),
-                hsl(${colors[1]} 100% 10%)
-                )`;
-document.querySelector("#square").style.backgroundColor = `hsl(${colors[2]} 100% 10%)`;
-
+/**
+ * All allowed hues
+ * @param {number[]} bases - Base hues to check against
+ * @returns {number[]} Hues that validate certain rules from a given base hue
+ */
 function allAllowedHues(bases) {
     return Array.from(new Array(360)) // Hue is in degrees
         .map((_, i) => i + 1) // Insert numbers into the Array from 1 to 300
         .filter(h => // Filter the Array
-            Math.abs(bases - h) > 15 // Must be sufficiently different than the base
-            && Math.abs(bases - h) <= 100 // Must not be too different
-            && (h < 50 || h > 150) // Must not be green
-        );
+            bases.every(base => // Check if every base is compatible with this hue
+                Math.abs(base - h) > 15 // Must be sufficiently different than the base
+                && Math.abs(base - h) <= 100 // Must not be too different
+                && (h < 50 || h > 150) // Must not be green
+            ));
 };
 
+/**
+ * Generate a hue that is compatible with given base hues
+ * @param {number[]} bases - Base hues to check against
+ * @return {number} A hue that is compatible with the given base hues
+ */
 function generateHue(...bases) {
     const validHues = allAllowedHues(bases);
     const index = Math.floor(Math.random() * validHues.length);
-    return allAllowedHues(bases)[index];
+    return validHues[index];
 };
+
+applyColors(generateColors());
